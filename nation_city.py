2730929c5746163275b24b38134bd8bca7622095
ChoiTuttle <eheh34w@gmail.com>
2021-06-01 22:22:40 +0900

    파파고 크롤링 연습

---
+++ nation_city.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import urllib.request,requests
+
+r = requests.get('https://backend.tripinu.com/nation/city/')
+
+ticket = r.json()
+print(ticket)
+
+for t in ticket:
+    client_id = "9MedZnoAL1W9OP_LfMl3" # 개발자센터에서 발급받은 Client ID 값
+    client_secret = "T4L4RASlfA" # 개발자센터에서 발급받은 Client Secret 값
+    encText = urllib.parse.quote(t['label'])
+    data = "source=en&target=ko&text=" + encText
+    url = "https://openapi.naver.com/v1/papago/n2mt"
+    request = urllib.request.Request(url)
+    request.add_header("X-Naver-Client-Id",client_id)
+    request.add_header("X-Naver-Client-Secret",client_secret)
+    response = urllib.request.urlopen(request, data=data.encode("utf-8"))
+    rescode = response.getcode()
+    if(rescode==200):
+        response_body = response.read()
+        print(response_body.decode('utf-8'))
+    else:
+        print("Error Code:" + rescode)
+
+
+
